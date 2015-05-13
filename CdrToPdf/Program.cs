7b6e7857ff060eb83213867cc169544adb1fab79
Designer <designer@polarfix.com.br>
2015-05-13 11:24:26 -0300

    create project

---
+++ CdrToPdf/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vagrant
+ * Date: 13/05/2015
+ * Time: 11:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CdrToPdf
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
